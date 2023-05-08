d5002499fe639221f1b4b4c033d522920fab0e93
Nguyễn Quốc Kha <72869710+Meeloow@users.noreply.github.com>
2023-05-08 21:56:16 +0700

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://agriculture-shop-lou1ti1od-meeloow.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://agriculture-shop-lou1ti1od-meeloow.vercel.app/";
