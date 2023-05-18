114fa57d87c8b7d2b77b86bb3347355d888ce4e3
Nguyễn Quốc Kha <72869710+Meeloow@users.noreply.github.com>
2023-05-19 00:47:55 +0700

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://agriculture-shop-toli.vercel.app//api/v2";
+export const server = "agriculture-shop-toli-gvyfb608r-meeloow.vercel.app/api/v2";
 
 export const backend_url = "https://agriculture-shop-toli.vercel.app/";
